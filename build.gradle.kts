f50d49d127c0ebb5d804106f89cd3e2fae348ddf
1fexd <PGFvy85nC@protonmail.com>
2023-05-03 21:55:29 +0200

    chore: Exclude fetch script from jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,17 @@ version = versioning.info.tag ?: versioning.info.full ?: "0.0.0"
 
 repositories {
     mavenCentral()
-    maven(url="https://jitpack.io")
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
     api("com.gitlab.grrfe:GSONKtExtensions:2.4.0")
     api("com.google.code.gson:gson:2.10.1")
     testImplementation(kotlin("test"))
+}
+
+tasks.withType<Jar> {
+    exclude("fetch_latest_libredirect.sh")
 }
 
 tasks.test {
