98770bf51586d03b4500936e0659bbedcbc89135
1fexd <PGFvy85nC@protonmail.com>
2024-06-22 19:47:16 +0200

    chore: Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.23" apply false
+    kotlin("jvm") version "1.9.24" apply false
     id("com.gitlab.grrfe.common-gradle-plugin") apply false
     id("app.cash.zipline") version "1.13.0"
 }
