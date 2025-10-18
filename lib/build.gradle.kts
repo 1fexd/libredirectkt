bce7191a3b95caf56df9a83315e1e8973b420046
1fexd <PGFvy85nC@protonmail.com>
2025-10-18 11:06:21 +0200

    feat: Add gradle tasks

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,4 +1,8 @@
 import fe.build.dependencies.Grrfe
+import fe.buildsrc.task.MetadataGeneratorTask
+import fe.buildsrc.task.UpdateConfigTask
+import fe.buildsrc.task.UpdateInstancesTask
+import org.gradle.kotlin.dsl.register
 
 dependencies {
     implementation(platform(Grrfe.std.bom))
@@ -20,4 +24,32 @@ dependencies {
     testImplementation(KotlinX.coroutines.test)
     testImplementation("com.willowtreeapps.assertk:assertk:_")
     testImplementation(kotlin("test"))
+}
+
+
+val generatedSrcDir: File = layout.buildDirectory.dir("generated/sources/metadata/main/java").get().asFile
+
+val main by sourceSets
+main.java.srcDir(generatedSrcDir)
+
+val generateMetadata = tasks.register<MetadataGeneratorTask>("generateMetadata") {
+    group = "build"
+    dir = generatedSrcDir
+}
+
+val assemble by tasks
+assemble.dependsOn(generateMetadata)
+
+val updateInstances = tasks.register<UpdateInstancesTask>("updateInstances") {
+    group = "update"
+    file.set(project.file("src/main/resources/libredirect_instances.json"))
+}
+val updateConfig = tasks.register<UpdateConfigTask>("updateConfig") {
+    group = "update"
+    file.set(project.file("src/main/resources/libredirect_config.json"))
+}
+
+val updateAll = tasks.register("updateAll") {
+    group = "update"
+    dependsOn(updateConfig, updateInstances)
 }
